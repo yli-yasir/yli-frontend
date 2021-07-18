01c65372ad60e9a162f3c28878ba9650d9cff3d5
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2021-07-18 11:32:12 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from '@apollo/client';
+import { GET_EXCHANGE_RATES } from './graphql/operations';
 
 function App() {
+  const { loading, error, data } = useQuery(GET_EXCHANGE_RATES);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {data && data.rates.map(({ currency, rate }) => (
+        <div key={currency}>
+          <p>
+            {currency}: {rate}
+          </p>
+        </div>
+      ))}
+      <div>Hello</div>
+    </>
   );
 }
 

b4199889be8ed8a2f7e4c7971bf3363ae51521ce
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2021-08-18 08:22:17 +0300

    fetch more until fill screen height

--- src/components/RepoList/index.js
+++ src/components/RepoList/index.js
@@ -5,52 +5,55 @@ import { TransitionGroup } from "react-transition-group";
 import Zoom from "../Zoom";
 import { makeProjectLink } from "../../utills";
 import LoadingPresenter from "../LoadingPresenter";
-import {InView} from "react-intersection-observer";
+import { useInView } from "react-intersection-observer";
 
 export default function RepoList() {
-
   const { loading, error, data, fetchMore } = useQuery(GET_REPOSITORIES, {
     variables: { first: 5 },
-    notifyOnNetworkStatusChange:true
+    notifyOnNetworkStatusChange: true,
   });
 
-  const repos = data?.viewer.repositories.edges.map(edge=>edge.node);
-  
-  function requestFetchMore(){
-    const pageInfo = data?.viewer.repositories.pageInfo;
+  const { inView, ref } = useInView();
 
-    if (pageInfo?.hasNextPage){
+  const repos = data?.viewer.repositories.edges.map((edge) => edge.node);
+  const pageInfo = data?.viewer.repositories.pageInfo;
+
+  function requestFetchMore() {
+    if (pageInfo?.hasNextPage) {
       fetchMore({
-        variables: { after: pageInfo.endCursor,first: 5 },
-      })
+        variables: { after: pageInfo.endCursor, first: 5 },
+      });
     }
   }
 
-  console.log(loading);
-
   return (
-    <LoadingPresenter loading={loading} error={error}>
-      <TransitionGroup appear={true}>
-        {repos &&
-          repos.map((repo, index) => {
-            const topics = repo.repositoryTopics.nodes.map(
-              ({ topic }) => topic.name
-            );
-            return (
-              <Zoom timeout={(index + 1) * 500} key={repo.name}>
-                <Card
-                  title={repo.name}
-                  link={makeProjectLink(repo.name, repo.defaultBranchRef.name)}
-                  description={repo.description}
-                  date={new Date(repo.updatedAt)}
-                  topics={topics}
-                />
-              </Zoom>
-            );
-          })}
-      </TransitionGroup>
-      <InView onChange={(visible)=> visible && requestFetchMore()}/>
-
-    </LoadingPresenter>
+    <>
+      <LoadingPresenter loading={loading} error={error}>
+        <TransitionGroup appear={true}>
+          {repos &&
+            repos.map((repo, index) => {
+              const topics = repo.repositoryTopics.nodes.map(
+                ({ topic }) => topic.name
+              );
+              return (
+                <Zoom timeout={(index + 1) * 250} key={repo.name}>
+                  <Card
+                    title={repo.name}
+                    link={makeProjectLink(
+                      repo.name,
+                      repo.defaultBranchRef.name
+                    )}
+                    description={repo.description}
+                    date={new Date(repo.updatedAt)}
+                    topics={topics}
+                  />
+                </Zoom>
+              );
+            })}
+        </TransitionGroup>
+      </LoadingPresenter>
+      <div ref={ref}></div>
+      {!loading && inView && pageInfo.hasNextPage && requestFetchMore()}
+    </>
   );
 }
